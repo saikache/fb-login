093d003953dc27e848df583e107dee8afff9ddba
saikache <saisagarkache@gmail.com>
2014-10-17 17:19:09 +0530

    sessions controller

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 
 OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '362961250538950','13d0ab205733fe9282dada29444e23f4'
+  provider :facebook, '362961250538950','4e23f4'
 end
